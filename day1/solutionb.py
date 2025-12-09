4fc6e6ed80d1f4642ba435bd85b4308edc25e392
jkahuja <jasmine.ahuja11@gmail.com>
2025-12-09 09:19:44 -0500

    fix part 2

--- day1/solutionb.py
+++ day1/solutionb.py
@@ -7,15 +7,19 @@ cur_pos = 50
 for line in sys.stdin:
     direction = line[0]
     turns = int(line[1:])
-    if direction == 'L':
-        new_pos = (cur_pos - turns) % 100
-    else:
-        new_pos = (cur_pos + turns) % 100
-    through_zero_left = new_pos > cur_pos and direction == 'L'
-    through_zero_right = cur_pos > new_pos and direction == 'R'
-    many_turns = turns >= 99
-    if new_pos == 0 or ((through_zero_left or through_zero_right) and cur_pos != 0) or many_turns:
-        num_zeros += 1
-    cur_pos = new_pos
+    for i in range(0, turns):
+        if direction == 'L':
+            cur_pos -= 1
+            if cur_pos == -1:
+                cur_pos = 99
+            if cur_pos == 0:
+                num_zeros += 1
+        else:
+            cur_pos += 1
+            if cur_pos == 100:
+                cur_pos = 0
+            if cur_pos == 0:
+                num_zeros += 1
+                
 
 print(num_zeros)
