09d62e428c558063692734ba82f4116b8b1b6df5
jkahuja <jasmine.ahuja11@gmail.com>
2025-12-10 17:21:31 -0500

    day 3

---
+++ day3/solution.py
@@ -0,0 +1,22 @@
+import sys
+
+total = 0
+
+for line in sys.stdin:
+    arr = [int(ch) for ch in line.strip()]
+    max_num = arr[0]
+    max_ind = 0
+    for idx, num in enumerate(arr[1:-1]):
+        if num > max_num:
+            max_num = num
+            max_ind = idx + 1
+    second_max_ind = max_ind + 1
+    second_max_num = arr[second_max_ind]
+    for num in arr[second_max_ind + 1:]:
+        second_max_num = max(second_max_num, num)
+    total += int(str(max_num) + str(second_max_num))
+    
+        
+
+print(total)
+    
