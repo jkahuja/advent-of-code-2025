7d019ff129b41943acd8a0df2838b5afbb2625fd
Jasmine Ahuja <jasmineahuja@Jasmines-MacBook-Pro.local>
2025-12-23 18:36:49 -0500

    avoid extra work

--- day10/solution.py
+++ day10/solution.py
@@ -1,16 +1,18 @@
 import sys
 import re
+import copy
 
 def bfs(goal_state, buttons):
+    visited = set()
     queue = []
     cur_state = '.' * len(goal_state)
     for button in buttons:
         queue.append((button, cur_state, 0))
     while True:
-        button, cur_state, num_presses = queue.pop(0)
+        cur_button, cur_state, num_presses = queue.pop(0)
         new_state = ""
         for i in range(len(cur_state)):
-            if i in button:
+            if i in cur_button:
                 if cur_state[i] == '.':
                     new_state += '#'
                 else:
@@ -19,8 +21,13 @@ def bfs(goal_state, buttons):
                 new_state += cur_state[i]
         if new_state == goal_state:
             return num_presses + 1
-        for button in buttons:
-            queue.append((button, new_state, num_presses + 1))
+        for new_button in buttons:
+            if new_button == cur_button:
+                continue
+            if ((tuple(new_button), new_state, num_presses + 1)) in visited:
+                continue
+            queue.append((new_button, new_state, num_presses + 1))
+            visited.add((tuple(new_button), new_state, num_presses + 1))
             
 
 total = 0
